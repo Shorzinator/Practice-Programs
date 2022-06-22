866f46ef770a823c31a3a4c12e738fb26e4fb2fb
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2022-06-22 16:08:32 +0530

    Optimizing bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,15 @@
 def bubbleSort(array):
     isSorted = False
+    counter = 0
+
     while not isSorted :
         isSorted = True
-        for i in range(len(array) - 1) :
+        for i in range(len(array) - 1 - counter) :
             if array[i] > array[i + 1] :
                 temp = array[i]
                 array[i] = array[i + 1]
                 array[i + 1] = temp
                 isSorted = False
+         counter += 1       
+         
     return array
