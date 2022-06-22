01a61cfdbcc997bd2de8fe44bbd5d637a6930a38
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2022-06-22 16:10:48 +0530

    Correcting function call

--- findThreeLargestNumbers.py
+++ findThreeLargestNumbers.py
@@ -26,7 +26,7 @@ array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
 
 """
 length = int(input()          # Length of the custom array for input 
-array = [int(input()) for i in range(len(length))]
+array = [int(input()) for i in range(length)]
 """
 
 print("Final result:", tln(array))
