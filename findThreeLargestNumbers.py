ad1b1def62a675d41b1935505911c113867ef39e
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2022-06-22 15:58:49 +0530

    update findThreeLargestNumbers.py

--- findThreeLargestNumbers.py
+++ findThreeLargestNumbers.py
@@ -25,7 +25,6 @@ def tln(array) :
 array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
 
 """
-
 length = int(input()          # Length of the custom array for input 
 array = [int(input()) for i in range(len(length))]
 """
